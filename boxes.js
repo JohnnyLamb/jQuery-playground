3ab1ec3943af645f72e7ee54a9e34d00ed6e7db1
johnny lamb <agnusjack@gmail.com>
2015-07-28 14:12:00 -0600

    first commit

--- boxes.js
+++ boxes.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+$(function() {
+  console.log("ready!");
+  $("#secretBox").css({
+
+    "background-color": "white",
+    "color": "black"
+  });
+  $("#secretBox").append("<h1>secret box!</h1>");
+  $('#row1').children().removeClass();
+  $('#row1').children().attr('class','box boxType3');
+  $('#row4').children().last().hide();
+  $('.boxType1').css('background-color','white');
+  $('#row2 div:lt(2)').css('visibility','hidden');
+  $(".box").not("#secretBox").css('width','2px');
+});
